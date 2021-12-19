f11a50e072fd94ea800451d3a683fd1ed7fef8c4
simontree <simonroof@tutanota.com>
2021-12-19 19:48:37 +0100

    Heroku

--- db/seeds/init_trips.ts
+++ db/seeds/init_trips.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 const trip = [
   {
     trip_id: "4f2b7664-5ea9-11ec-bf63-0242ac130002",
-    tripname: "Urlaub auf Hawaii",
+    name: "Urlaub auf Hawaii",
     start: "2022-10-10",
     end: "2022-10-21",
     country: "Hawaii",
