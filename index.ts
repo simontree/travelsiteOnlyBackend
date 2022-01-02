80033c78b0effde0fa7603da59934023ff34253f
Dohruba <hendrichsricardo@gmail.com>
2022-01-02 18:30:44 +0100

    Working on Cookies

--- index.ts
+++ index.ts
@@ -189,7 +189,7 @@ app.post("/login", async (req, res) => {
   }
    res.cookie("session", sessionId, {
      maxAge: 60 * 60 * 1000,
-     httpOnly: false,
+     httpOnly: true,
      sameSite: "none",
      secure: process.env.NODE_ENV === "development",
    });
