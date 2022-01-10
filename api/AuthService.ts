931a24c861503de3c2d2a1645e4ea94646dc9623
simontree <simonroof@tutanota.com>
2022-01-10 12:45:12 +0100

    redis activated again

--- api/AuthService.ts
+++ api/AuthService.ts
@@ -6,12 +6,12 @@ import { createClient } from "redis";
 import crypto from "crypto";
 
 import { promisify } from "util";
-// const redisPass = "hlzu8VsbpKUSe9GysuZDJQN73rDhipVy";
+const redisPass = "hlzu8VsbpKUSe9GysuZDJQN73rDhipVy";
 
 const client = createClient({
   url: process.env.REDIS_URL,
-  // no_ready_check: true,
-  // auth_pass: redisPass,
+  no_ready_check: true,
+  auth_pass: redisPass,
 });
 //const client = createClient();
 
